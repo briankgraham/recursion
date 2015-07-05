13728d16381748543d0284bcc569d4b0a636be57
Brian Graham <graham.briank@gmail.com>
2015-07-05 13:45:45 -0700

    Checks pass for arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,9 +9,17 @@ var stringifyJSON = function(obj) {
   if (obj === null){ return 'null';}
   if (typeof obj === 'boolean') return obj.toString();
   if (typeof obj === 'number') return obj.toString();
+  if (typeof obj === 'string') return '"' + obj + '"';
   if (Array.isArray(obj)){
   	return '[' + _.map(obj, function(item){
   		return stringifyJSON(item);
   	}).join(',') + ']';
+  }
+  if (typeof obj === 'object'){
+  	var result = [];
+  	_.each(obj, function(value, key){
+  		if (value){ result.push('"' + key + '":' + '"' + value + '"');}
+  	});
+  	return '{' + result.join(',') + '}';
   }
 };
